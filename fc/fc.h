813df7c2a4f9fab781a75e3f3711a83c0b90f42d
Leo Adberg <leo@adberg.com>
2019-03-15 01:30:39 -0700

    Formatted better for SIL

--- fc/fc.h
+++ fc/fc.h
@@ -1,3 +1,7 @@
+#include "msg_downlink_generated.h"
+#include "msg_uplink_generated.h"
+#include "msg_fc_update_generated.h"
+
 #include "mbed.h"
 #include "pins.h"
 
@@ -16,80 +20,95 @@ void buildCurrentMessage();
 Timer msgTimer;
 FlatBufferBuilder builder(BUF_SIZE);
 bool bpIgnited[NUM_BP];
-int main() {
-    DigitalOut led_red(STATE_LED_RED);
-    DigitalOut led_green(STATE_LED_GREEN);
+
+
+DigitalOut led_red(STATE_LED_RED);
+DigitalOut led_green(STATE_LED_GREEN);
+
+Serial rs422(RS422_TX, RS422_RX);
+Serial debug_uart(DEBUG_TX, DEBUG_RX);
+
+us_timestamp_t last_msg_send_us;
+
+void loop() {
+    // Send a message every MSG_SEND_INTERVAL_US microseconds
+    us_timestamp_t current_time = msgTimer.read_high_resolution_us();
+    if (current_time >= last_msg_send_us + MSG_SEND_INTERVAL_US) {
+        uint8_t* buf = builder.GetBufferPointer();
+        int size = builder.GetSize();
+
+        // Write the size of the message
+        // void* size_ptr = (void*)(&size);
+        // rs422.write((uint8_t*)size_ptr, sizeof(size), NULL);
+        // then write the message
+        rs422.write(buf, size, NULL);
+
+        // also just toggle the red LED at this point
+        led_red = (led_red == 0) ? 1 : 0;
+
+        debug_uart.printf("Sending FC update\r\n");
+
+        last_msg_send_us = current_time;
+    }
+    // Always read messages
+    if (rs422.readable()) {
+        // Read into message type
+        const UplinkMsg *msg = getUplinkMsg(rs422.getc());
+        if (msg) {
+            UplinkType type = msg->Type();
+
+            // If turning on/off black powder, for now we just set our state
+            // (in the future we will actually do black powder stuff)
+            if (type == UplinkType_BlackPowderOn || type == UplinkType_BlackPowderOff) {
+                const Vector<uint8_t> *bp = msg->BP();
+                for (int i = 0; i < NUM_BP; i++) {
+                    if (bp->Get(i)) {
+                        bpIgnited[i] = (type == UplinkType_BlackPowderOn);
+                    }
+                }
+
+                // Update the message being sent out to T/PC
+                buildCurrentMessage();
+            }
+        }
+    }
+
+    if (debug_uart.readable()) {
+        char c = debug_uart.getc();
+
+        if (c == 'r') {
+            led_red = (led_red == 0) ? 1 : 0;
+        } else if (c == 'g') {
+            led_green = (led_green == 0) ? 1 : 0;
+        }
+    }
+}
+
+void start() {
     led_red = 0;
     led_green = 1;
 
     msgTimer.start();
 
-    Serial rs422(RS422_TX, RS422_RX);
     rs422.baud(RS422_BAUDRATE);
     rs422.set_blocking(false);
 
-    Serial debug_uart(DEBUG_TX, DEBUG_RX);
     debug_uart.baud(DEBUG_UART_BAUDRATE);
     debug_uart.set_blocking(false);
     debug_uart.printf("---- CalSTAR Flight Computer ----\r\n");
 
     buildCurrentMessage();
 
-    us_timestamp_t last_msg_send_us = msgTimer.read_high_resolution_us();
-    while (true) {
-        // Send a message every MSG_SEND_INTERVAL_US microseconds
-        us_timestamp_t current_time = msgTimer.read_high_resolution_us();
-        if (current_time >= last_msg_send_us + MSG_SEND_INTERVAL_US) {
-            uint8_t* buf = builder.GetBufferPointer();
-            int size = builder.GetSize();
-
-            // Write the size of the message
-            // void* size_ptr = (void*)(&size);
-            // rs422.write((uint8_t*)size_ptr, sizeof(size), NULL);
-            // then write the message
-            rs422.write(buf, size, NULL);
-
-            // also just toggle the red LED at this point
-            led_red = (led_red == 0) ? 1 : 0;
-
-            debug_uart.printf("Sending FC update\r\n");
-
-            last_msg_send_us = current_time;
-        }
-        // Always read messages
-        if (rs422.readable()) {
-            // Read into message type
-            const UplinkMsg *msg = getUplinkMsg(rs422.getc());
-            if (msg) {
-                UplinkType type = msg->Type();
-
-                // If turning on/off black powder, for now we just set our state
-                // (in the future we will actually do black powder stuff)
-                if (type == UplinkType_BlackPowderOn || type == UplinkType_BlackPowderOff) {
-                    const Vector<uint8_t> *bp = msg->BP();
-                    for (int i = 0; i < NUM_BP; i++) {
-                        if (bp->Get(i)) {
-                            bpIgnited[i] = (type == UplinkType_BlackPowderOn);
-                        }
-                    }
-
-                    // Update the message being sent out to T/PC
-                    buildCurrentMessage();
-                }
-            }
-        }
-
-        if (debug_uart.readable()) {
-            char c = debug_uart.getc();
-
-            if (c == 'r') {
-                led_red = (led_red == 0) ? 1 : 0;
-            } else if (c == 'g') {
-                led_green = (led_green == 0) ? 1 : 0;
-            }
-        }
-    }
+    last_msg_send_us = msgTimer.read_high_resolution_us();
 }
+
+int main() {
+  start();
+  while (1) {
+    loop();
+  }
+}
+
 
 const UplinkMsg *getUplinkMsg(char c) {
     static uint8_t buffer[BUF_SIZE];
