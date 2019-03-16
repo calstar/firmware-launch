eb570f5ecbaf2d0ea7ed943523a0fdb806b756a6
Jacob Posner <jashaszun123@gmail.com>
2019-03-16 00:02:56 -0700

    Fixed flatbuffer receive functions.

--- fc/fc.h
+++ fc/fc.h
@@ -9,7 +9,7 @@
 #define RS422_BAUDRATE (115200)
 #define MSG_SEND_INTERVAL_US (100000u) // 100*1000us = 100ms
 #define NUM_BP (7)
-#define BUF_SIZE (1024)
+#define BUF_SIZE (256)
 
 using namespace flatbuffers;
 using namespace Calstar;
@@ -112,36 +112,83 @@ int main() {
 
 const UplinkMsg *getUplinkMsg(char c) {
     static uint8_t buffer[BUF_SIZE];
+    static uint8_t return_buffer[BUF_SIZE];
     static unsigned int len = 0;
-    // static unsigned int messageSize = 0;
 
     if (len == BUF_SIZE) {
-        len = 0;
+        // If at end of buffer, shift and add to end
+        memmove(buffer, buffer + 1, BUF_SIZE-1);
+        buffer[BUF_SIZE-1] = (uint8_t)c;
+    } else {
+        // Otherwise build up buffer
+        buffer[len++] = (uint8_t)c;
     }
-    buffer[len++] = (uint8_t)c;
-    Verifier v(buffer, len);
-    if (VerifyUplinkMsgBuffer(v)) {
-        len = 0;
-        return GetUplinkMsg(buffer);
+
+    // The verifier will say that buf has a valid message for any length from actual_length-some number to full buffer length
+    // So basically, we trust the verifier, but verify separately by having a #-bytes field in the message itself
+    // So if the verifier says there's a valid message in the buffer, we read that message, get the number of bytes that the
+    // message says it should be, and actually process a message of THAT size.
+    Verifier verifier(buffer, len);
+    if (VerifyUplinkMsgBuffer(verifier)) {
+        memcpy(return_buffer, buffer, len);
+        const UplinkMsg *output = GetUplinkMsg(return_buffer);
+        // The message knows how big it should be
+        uint8_t expectedBytes = output->Bytes();
+
+        uint8_t actual_len = len;
+        if (len < expectedBytes) {
+            // The verifier will say we have a valid message even if we're a few bytes short
+            // Just read more characters at this point by returning early
+            return NULL;
+        } else if (len > expectedBytes) {
+            // Now we want to verify that the "smaller buffer" with length equal to the expected number of bytes
+            // is actually a message in its own right (just a double check basically)
+            Verifier smallerVerifier(buffer, expectedBytes);
+            if (VerifyUplinkMsgBuffer(smallerVerifier)) {
+                actual_len = expectedBytes;
+                // If it is a message, then make the return buffer just hold it (clear the extra bytes we copied)
+                memset(return_buffer+actual_len, 0, len - actual_len);
+            } else {
+                // If it isn't valid, then this buffer just has some malformed messages... continue and let's get
+                // them out of the buffer by reading more
+                return NULL;
+            }
+        }
+
+        // Now that we've read a valid message, remove it from the buffer and move everything else down
+        // Then reduce current buffer length by the length of the processed message
+        // Then clear the rest of the buffer so that we don't get false positives with the verifiers
+        memmove(buffer, buffer+actual_len, BUF_SIZE-actual_len);
+        len -= actual_len;
+        memset(buffer+len, 0, BUF_SIZE-len);
+
+        return output;
     }
     return NULL;
-    // if (pos == sizeof(int)) {
-    //     // Read the size of the incoming message
-    //     void *buf = buffer;
-    //     messageSize = *((int*)buf);
-    // } else if (pos == sizeof(int) + messageSize) {
-    //     // Reached end of message
-    //     void *result = buffer + sizeof(int);
-    //     pos = 0;
-    //     messageSize = 0;
-    //     return result;
-    // }
-    //
-    // return NULL;
 }
 
 void buildCurrentMessage() {
+    builder.Reset();
     Offset<FCUpdateMsg> message = CreateFCUpdateMsg(builder,
+        1, // Can't be 0 or it will be ignored
+        FCState_Pad,
+        0.0f, 1.0f, 2.0f,
+        3.0f, 4.0f, 5.0f,
+        6.0f, 7.0f, 8.0f,
+        9.0f, 10.0f,
+        bpIgnited[0], false,
+        bpIgnited[1], true,
+        bpIgnited[2], false,
+        bpIgnited[3], true,
+        bpIgnited[4], false,
+        bpIgnited[5], true,
+        bpIgnited[6], false);
+    builder.Finish(message);
+
+    uint8_t bytes = (uint8_t)builder.GetSize();
+    builder.Reset();
+    message = CreateFCUpdateMsg(builder,
+        bytes, // Fill in actual number of bytes
         FCState_Pad,
         0.0f, 1.0f, 2.0f,
         3.0f, 4.0f, 5.0f,
--- tpc/tpc.h
+++ tpc/tpc.h
@@ -7,8 +7,8 @@
 
 #define DEBUG_UART_BAUDRATE (115200)
 #define RS422_BAUDRATE (115200)
-#define MSG_SEND_INTERVAL_US (200000u) // 200*1000us = 200ms
-#define BUF_SIZE (1024)
+#define MSG_SEND_INTERVAL_US (250000u) // 250*1000us = 250ms
+#define BUF_SIZE (256)
 
 using namespace flatbuffers;
 using namespace Calstar;
@@ -84,38 +84,121 @@ int main() {
 
 const UplinkMsg *getUplinkMsg(char c) {
     static uint8_t buffer[BUF_SIZE];
+    static uint8_t return_buffer[BUF_SIZE];
     static unsigned int len = 0;
-    // static unsigned int messageSize = 0;
 
     if (len == BUF_SIZE) {
-        len = 0;
+        // If at end of buffer, shift and add to end
+        memmove(buffer, buffer + 1, BUF_SIZE-1);
+        buffer[BUF_SIZE-1] = (uint8_t)c;
+    } else {
+        // Otherwise build up buffer
+        buffer[len++] = (uint8_t)c;
     }
-    buffer[len++] = (uint8_t)c;
-    Verifier v(buffer, len);
-    if (VerifyUplinkMsgBuffer(v)) {
-        len = 0;
-        return GetUplinkMsg(buffer);
+
+    // The verifier will say that buf has a valid message for any length from actual_length-some number to full buffer length
+    // So basically, we trust the verifier, but verify separately by having a #-bytes field in the message itself
+    // So if the verifier says there's a valid message in the buffer, we read that message, get the number of bytes that the
+    // message says it should be, and actually process a message of THAT size.
+    Verifier verifier(buffer, len);
+    if (VerifyUplinkMsgBuffer(verifier)) {
+        memcpy(return_buffer, buffer, len);
+        const UplinkMsg *output = GetUplinkMsg(return_buffer);
+        // The message knows how big it should be
+        uint8_t expectedBytes = output->Bytes();
+
+        uint8_t actual_len = len;
+        if (len < expectedBytes) {
+            // The verifier will say we have a valid message even if we're a few bytes short
+            // Just read more characters at this point by returning early
+            return NULL;
+        } else if (len > expectedBytes) {
+            // Now we want to verify that the "smaller buffer" with length equal to the expected number of bytes
+            // is actually a message in its own right (just a double check basically)
+            Verifier smallerVerifier(buffer, expectedBytes);
+            if (VerifyUplinkMsgBuffer(smallerVerifier)) {
+                actual_len = expectedBytes;
+                // If it is a message, then make the return buffer just hold it (clear the extra bytes we copied)
+                memset(return_buffer+actual_len, 0, len - actual_len);
+            } else {
+                // If it isn't valid, then this buffer just has some malformed messages... continue and let's get
+                // them out of the buffer by reading more
+                return NULL;
+            }
+        }
+
+        // Now that we've read a valid message, remove it from the buffer and move everything else down
+        // Then reduce current buffer length by the length of the processed message
+        // Then clear the rest of the buffer so that we don't get false positives with the verifiers
+        memmove(buffer, buffer+actual_len, BUF_SIZE-actual_len);
+        len -= actual_len;
+        memset(buffer+len, 0, BUF_SIZE-len);
+
+        return output;
     }
     return NULL;
 }
 const FCUpdateMsg *getFCUpdateMsg(char c) {
     static uint8_t buffer[BUF_SIZE];
+    static uint8_t return_buffer[BUF_SIZE];
     static unsigned int len = 0;
 
     if (len == BUF_SIZE) {
-        len = 0;
+        // If at end of buffer, shift and add to end
+        memmove(buffer, buffer + 1, BUF_SIZE-1);
+        buffer[BUF_SIZE-1] = (uint8_t)c;
+    } else {
+        // Otherwise build up buffer
+        buffer[len++] = (uint8_t)c;
     }
-    buffer[len++] = (uint8_t)c;
-    Verifier v(buffer, len);
-    if (VerifyFCUpdateMsgBuffer(v)) {
-        len = 0;
-        return GetFCUpdateMsg(buffer);
+
+    // The verifier will say that buf has a valid message for any length from actual_length-some number to full buffer length
+    // So basically, we trust the verifier, but verify separately by having a #-bytes field in the message itself
+    // So if the verifier says there's a valid message in the buffer, we read that message, get the number of bytes that the
+    // message says it should be, and actually process a message of THAT size.
+    Verifier verifier(buffer, len);
+    if (VerifyFCUpdateMsgBuffer(verifier)) {
+        memcpy(return_buffer, buffer, len);
+        const FCUpdateMsg *output = GetFCUpdateMsg(return_buffer);
+        // The message knows how big it should be
+        uint8_t expectedBytes = output->Bytes();
+
+        uint8_t actual_len = len;
+        if (len < expectedBytes) {
+            // The verifier will say we have a valid message even if we're a few bytes short
+            // Just read more characters at this point by returning early
+            return NULL;
+        } else if (len > expectedBytes) {
+            // Now we want to verify that the "smaller buffer" with length equal to the expected number of bytes
+            // is actually a message in its own right (just a double check basically)
+            Verifier smallerVerifier(buffer, expectedBytes);
+            if (VerifyFCUpdateMsgBuffer(smallerVerifier)) {
+                actual_len = expectedBytes;
+                // If it is a message, then make the return buffer just hold it (clear the extra bytes we copied)
+                memset(return_buffer+actual_len, 0, len - actual_len);
+            } else {
+                // If it isn't valid, then this buffer just has some malformed messages... continue and let's get
+                // them out of the buffer by reading more
+                return NULL;
+            }
+        }
+
+        // Now that we've read a valid message, remove it from the buffer and move everything else down
+        // Then reduce current buffer length by the length of the processed message
+        // Then clear the rest of the buffer so that we don't get false positives with the verifiers
+        memmove(buffer, buffer+actual_len, BUF_SIZE-actual_len);
+        len -= actual_len;
+        memset(buffer+len, 0, BUF_SIZE-len);
+
+        return output;
     }
     return NULL;
 }
 
 void buildCurrentMessage() {
+    builder.Reset();
     Offset<FCUpdateMsg> fcUpdateMsg = CreateFCUpdateMsg(builder,
+        1, // Can't be 0 or it will be ignored
         FCState_Pad,
         0.0f, 1.0f, 2.0f,
         3.0f, 4.0f, 5.0f,
@@ -129,6 +212,32 @@ void buildCurrentMessage() {
         false, true,
         false, false);
     Offset<DownlinkMsg> message = CreateDownlinkMsg(builder,
+        1, // Can't be 0 or it will be ignored
+        TPCState_Pad,
+        (int)fcPower,
+        fcUpdateMsg, // TODO: use the latest FC data
+        builder.CreateString("gps test"),
+        123);
+    builder.Finish(message);
+
+    uint8_t bytes = (uint8_t)builder.GetSize();
+    builder.Reset();
+    fcUpdateMsg = CreateFCUpdateMsg(builder,
+        1, // TODO: does the byte count here matter, since it's a sub-message?
+        FCState_Pad,
+        0.0f, 1.0f, 2.0f,
+        3.0f, 4.0f, 5.0f,
+        6.0f, 7.0f, 8.0f,
+        9.0f, 10.0f,
+        false, false,
+        false, true,
+        false, false,
+        false, true,
+        false, false,
+        false, true,
+        false, false);
+    message = CreateDownlinkMsg(builder,
+        bytes, // Fill in actual number of bytes
         TPCState_Pad,
         (int)fcPower,
         fcUpdateMsg, // TODO: use the latest FC data
