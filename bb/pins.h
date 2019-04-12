5250b0513eb3761db1cc3e9968669b3ad1c5c33c
Aled Cuda <aledvirgil@gmail.com>
2019-04-12 16:27:27 -0700

    fix gitignore and add bb pin defs

--- bb/pins.h
+++ bb/pins.h
@@ -2,9 +2,16 @@
 
 #define I2C_SENSOR_SDA PB_7
 #define I2C_SENSOR_SCL PB_6
+
+#define KX222_MOSI PB_15
+#define KX222_MISO PB_14
+#define KX222_SCLK PB_13
+#define KX222_CS   PB_12
+
 #define STATE_LED_RED PC_8
 #define STATE_LED_GREEN PA_8
 #define STATE_LED_BLUE PC_9
+
 #define DEBUG_TX PA_2
 #define DEBUG_RX PA_3
 #define DEBUG_UART_BAUDRATE 115200
