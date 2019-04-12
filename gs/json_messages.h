911f295a2285c700a3a960a77810e5cbef6896ef
Neelay Junnarkar <neelay.junnarkar@gmail.com>
2019-04-12 12:10:22 -0700

    json output to computer

---
+++ gs/json_messages.h
@@ -0,0 +1,19 @@
+#ifndef JSON_MESSAGES_H
+#define JSON_MESSAGES_H
+
+#define VALUE           "value"
+#define ID              "id"
+#define TIMESTAMP       "timestamp"
+
+#define FC_PWRD         "fc.pwr"
+#define FC_STATE        "fc.state"
+#define FC_ALT          "fc.alt"
+#define TPC_GPS         "tpc.gps"
+#define GS_TRANSMISSION "gs.transmission"
+#define COMMS_RECD      "comms.recd"
+#define COMMS_SENT      "comms.sent"
+#define GS_RSSI         "gs.rssi"
+#define TPC_BATV        "tpc.bat_v"
+#define TPC_STATE       "tpc.state"
+
+#endif
