8af2715b8b95ae8ca6417e36b11ede50412b0199
Jaren Mendelsohn <jaren.mendelsohn@gmail.com>
2019-04-17 13:39:15 -0700

    Remove util.h, merged into fc.h

--- fc/fc.h
+++ fc/fc.h
@@ -5,8 +5,6 @@
 #include "mbed.h"
 #include "pins.h"
 
-#include "util.h"
-
 #include "MPL3115A2.h"
 
 // TODO: Turn off BP after certain time?
@@ -26,6 +24,35 @@
 
 using namespace flatbuffers;
 using namespace Calstar;
+
+enum LaunchState {
+    PRE_LAUNCH = 0b011,
+    ON_GROUND = 0b101,
+    LAUNCHED = 0b110,
+    PASSED_MIN_ALT = 0b001,
+    PASSED_APOGEE = 0b010,
+    PASSED_MAIN_CHUTE = 0b100
+};
+
+struct Node {
+    Node* previous;
+    Node* next;
+    double value;
+
+    Node(Node* p, double v) : previous(p), value(v) {}
+
+    static Node* createList(int length) {
+        Node* first = new Node(nullptr, 0);
+        Node* last = first; 
+        for (int i = 0; i < length; i++) {
+            last = new Node(last, 0);
+            last->previous->next = last;
+        }
+        first->previous = last;
+        last->next = first;
+        return first;
+    }
+};
 
 const UplinkMsg *getUplinkMsg(char c);
 void buildCurrentMessage();
@@ -52,6 +79,7 @@ us_timestamp_t last_msg_send_us;
 
 uint8_t rs422_read_buf[BUF_SIZE];
 UARTSerial rs422(RS422_TX, RS422_RX, RS422_BAUDRATE);
+
 
 /* 
  * Sets LED based on current state
